9dec4ba08200e329ea1c426cae4ddd83109ebbde
Brad Davis <brad.bdavis1@gmail.com>
2011-04-18 13:04:54 -0400

    finished states array

--- post.js
+++ post.js
@@ -11,15 +11,7 @@ var states = {
 AL:'Alabama',	AK:'Alaska',AZ:'Arizona',AR:'Arkansas',CA:'California',
 CO:'Colorado',CT:'Connecticut',DE:'Delaware',FL:'Florida',GA:'Georgia',
 HA:'Hawaii',ID:'Idaho',IL:'Illinois',IN:'Indiana',IA:'Iowa',
-KS:'Kansas',KY:'Kentucky',LA:'Louisiana,ME:'Maine',	Maryland,
-Massachusetts,	Michigan,	Minnesota,	Mississippi,
-Missouri,	Montana,	Nebraska,	Nevada,
-New Hampshire,	New Jersey,	New Mexico,	New York,
-North Carolina,	North Dakota,	Ohio,	Oklahoma,
-Oregon,	Pennsylvania,	Rhode Island,	South Carolina,
-South Dakota,	Tennessee,	Texas,	Utah,
-Vermont,	Virginia,	Washington,	West Virginia,
-Wisconsin,	 Wyoming}
+KS:'Kansas',KY:'Kentucky',LA:'Louisiana',ME:'Maine',MD:'Maryland',MA:'Massachusetts',MI:'Michigan',MN:'Minnesota',MS:'Mississippi',MO:'Missouri',MT:'Montana',NE:'Nebraska',NV:'Nevada',NH:'New Hampshire',NJ:'New Jersey',NM:'New Mexico',NY:'New York',NC:'North Carolina',ND:'North Dakota',OH:'Ohio',OK:'Oklahoma',OR:'Oregon',PA:'Pennsylvania',RH:'Rhode Island',SC:'South Carolina',SD:'South Dakota',TN:'Tennessee',TX:'Texas',UT:'Utah',VT:'Vermont',VA:'Virginia',WA:'Washington',WV:'West Virginia',WI:'Wisconsin',WY:'Wyoming'}
 
 
 server = http.createServer(function(req, res) {
